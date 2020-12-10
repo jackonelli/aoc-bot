92374ae801bdbc06376374aa1ea31ac478abfdd3
Jakob <jakob.lindqvist@chalmers.se>
2020-12-10 20:53:04 +0100

    Rust working ex

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,7 @@
-use std::env;
-use std::time::Duration;
-use serenity::{
-    async_trait,
-    model::{channel::Message, gateway::Ready},
-    prelude::*,
-};
-
-const API_DELAY: Duration = Duration::from_secs(5);
-const HELP_CMD: &str = "?help";
-const START_CMD: &str = "!start";
-
-struct Handler;
-
-#[async_trait]
-impl EventHandler for Handler {
-    async fn message(&self, ctx: Context, msg: Message) {
-        if msg.content == HELP_CMD {
-            if let Err(why) = msg.channel_id.say(&ctx.http, "Here's help").await {
-                println!("Error sending message: {:?}", why);
-            };
-        };
-        if msg.content == START_CMD {
-            loop {
-                if let Err(why) = msg.channel_id.say(&ctx.http, "Update").await {
-                    println!("Error sending message: {:?}", why);
-                };
-                tokio::time::delay_for(API_DELAY).await;
-            }
-        }
-    }
-    async fn ready(&self, _ctx: Context, ready: Ready) {
-        println!("{} is connected!", ready.user.name);
-    }
-
-}
-
+use aoc_bot::aoc_data::get_aoc_data;
 
 #[tokio::main]
 async fn main() {
-    let token = env::var("DISCORD_TOKEN")
-        .expect("Expected a token in the environment");
-
-    let mut client = Client::builder(&token)
-        .event_handler(Handler)
-        .await
-        .expect("Err creating client");
-
-    if let Err(why) = client.start().await {
-        println!("Client error: {:?}", why);
-    }
+    let latest_data = get_aoc_data();
+    println!("{:?}", latest_data.await);
 }
